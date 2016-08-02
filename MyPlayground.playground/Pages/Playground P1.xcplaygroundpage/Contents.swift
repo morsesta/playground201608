f8105a8f2945e8e8ee24d721f8c8a69e1601482e
Yun Chu Wang <morsesta@yunde-macbook-pro.local>
2016-08-02 17:25:50 +0800

    playground

---
+++ MyPlayground.playground/Pages/Playground P1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+
+var age = 19
+if age < 30 {
+    var message = "Legend"
+}
+
+else {
+    if age < 45 { a little bit old
+}
+
+    else {
+        if age < 50 {
+            var message = "a little bit older"
+        }
+}
+
+    else {
+        if age < 70 {
+            var message = "very old"
+}
+        else {
+            var message = "too old"
+}
+}
+}
+}
